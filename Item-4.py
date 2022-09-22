3ac449cf63f00238fa790cb51245b85304cfbe4f
Sebas <sebas@debian-BULLSEYE-live-builder-AMD64>
2022-09-22 00:13:00 -0400

    Simplex parameters

--- Item-4.py
+++ Item-4.py
@@ -1,13 +1,12 @@
-from statistics import variance
 import numpy as np
 
 def simplex(objective,restrictions,variables,maximize):
-
-    return variables
+    tuplaSolucion = (objective,restrictions,variables,maximize)
+    return tuplaSolucion
 
 objective = np.array([30.0, 100.0, 0, 0, 0, -100000.0])
-restrictions = np.matrix('1 2; 3 4')
-print(objective[0])
-restrictions
-
-print(type(objective))
+restrictions = np.matrix('1 1 1 0 0 0 7.0; 4.0,10.0,0,1,0,0,40.0;10.0 0 0 0 -1 1 30.0')
+variables = np.array(['x1', 'x2', 's1', 's2', 's3', 'a3'])
+maximize = "true"
+np.set_printoptions(suppress=True)
+print(simplex(objective,restrictions,variables,maximize))
