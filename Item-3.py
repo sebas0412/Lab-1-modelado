357bf8fa508c7e73015e9164196bb96bd9a72ada
gerardo-c <gerardo.camposbadilla@ucr.ac.cr>
2022-09-21 20:34:39 -0600

    Corregido valores de retorno de items 1 y 2

--- Item-3.py
+++ Item-3.py
@@ -1,24 +1,49 @@
 import Lexer
+import numpy as nump
+from numpy import linalg as lina
+from numpy.linalg import inv
 from Clases import Equation, MathExpression, MathRestriction
+
+
 def parse_equation(equation):
-    return Lexer.plyParse(equation)
+    thisEquation = Lexer.plyParse(equation)
+    dictionary = {}
+
+    for item in thisEquation.dictionary:
+        dictionary[item.coeff] = item.variable
+    return dictionary
+
 def parse_restriction(restriction):
-    return parse_equation(restriction)
+    thisEquation = Lexer.plyParse(restriction)
+    dictionary = {}
+    isUpperBound = False
+
+    for item in thisEquation.dictionary:
+        dictionary[item.coeff] = item.variable
+
+    if (thisEquation.restriction.type == "<="):
+        isUpperBound = True
+    return dictionary, thisEquation.restriction.value, isUpperBound
 
 def parse_problem(objective,restrictions, maximize):
-    restrictionList = []
-    for item in restrictions:
-        restrictionList.insert(len(restrictionList), parse_restriction(item))
-    for item in restrictionList:
-        item.printEquation()
+    parsedObjective = parse_equation(objective)
 
+    rest_dictionary = []
+    rest_value = []
+    rest_upperBound = []
 
+    for index in range (0, len(restrictions)):
+        dictionary, restrictionValue, isUpperBound = parse_restriction(restrictions[index])
+        rest_dictionary.insert(index, dictionary)
+        rest_value.insert(index, restrictionValue)
+        rest_upperBound.insert(index, isUpperBound)
 
     tablaSimplex = [],[]
     varNames = []
 
 
 parse_problem("30x1 + 100x2", ["-x1 + x2 <= 7", "4x1 + 10x2 <= 40", "10x1 >= 30"], True)
+
 
 
 
