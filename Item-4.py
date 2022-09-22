88d6f6c4574092b01d24f2911825802c1834ebbb
Sebas <sebas@debian-BULLSEYE-live-builder-AMD64>
2022-09-21 23:29:32 -0400

    Merge

---
+++ Item-4.py
@@ -0,0 +1,13 @@
+from statistics import variance
+import numpy as np
+
+def simplex(objective,restrictions,variables,maximize):
+
+    return variables
+
+objective = np.array([30.0, 100.0, 0, 0, 0, -100000.0])
+restrictions = np.matrix('1 2; 3 4')
+print(objective[0])
+restrictions
+
+print(type(objective))
