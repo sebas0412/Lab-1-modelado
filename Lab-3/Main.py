b8defbaa7d65f2fed3bf1dd5c75d19f1286e4055
Gerardo Campos <gerardo.camposbadilla@ucr.ac.cr>
2022-11-09 23:54:11 -0600

    Item 7 y 8

--- Lab-3/Main.py
+++ Lab-3/Main.py
@@ -1,7 +1,26 @@
+# Pregunta 8a
+# ¿Por qué la probabilidad de formar un nombre parece aumentar conforme n incrementa?
+# Porque las secuencias más grandes ya son bloques más grandes y pueden tener incluso palabras
+# ya formadas que se usan para construir nuevas palabras, no hay que ir solo por todas las
+# combinaciones posibles como podría ser cuando se va letra por letra.
+
+
+
+
 from Markov import Markov
 
 markov = Markov()
-markov.load_words("words.txt")
-model = markov.create_model(markov.wordsArray,1)
-markov.generate_word(model, 17)
+
+# Pregunta 8b
+# Semilla 142, con secuencia tamano 1
+markov.load_words("pokemon.csv")
+model = markov.create_model(markov.wordsArray, 1)
+print(markov.generate_word(model, 142))
+# print(markov.get_probability(model, "mew"))
 ##print(transition, sequences)
+
+# Nombre que no tiene sentido
+# Semilla 1335, con secuencia tamano 1
+markov.load_words("pokemon.csv")
+model = markov.create_model(markov.wordsArray, 1)
+print(markov.generate_word(model, 1335))
--- Lab-3/Markov.py
+++ Lab-3/Markov.py
@@ -23,7 +23,7 @@ class Markov:
         for line in file:
             for word in line.split():
                 if word != ' ' or '\n' or '\t':
-                    self.wordsArray.append(word)
+                    self.wordsArray.append(word.lower())
 
     def add_decorators(self, words, decorator, n):
         decor: str = ""
@@ -36,6 +36,17 @@ class Markov:
             decoratedWords.append(decor + item + decor)
 
         return decoratedWords
+
+    def add_decorators_string(self, word:str, decorator, n):
+        decor: str = ""
+        decoratedWord = ""
+
+        for index in range(n):
+            decor += decorator
+
+        decoratedWord = decor + word + decor
+
+        return decoratedWord
 
     def get_sequences(self, words, k):
         combined = ""
@@ -89,7 +100,9 @@ class Markov:
                 total += entry.amount
             if total > 0:
                 for x in range(len(dictionary)):
-                    matrix[seq][x] = (1 / total) * dictionary[x].amount
+                    if (dictionary[x].amount > 0):
+                        result = (1 / total) * dictionary[x].amount
+                        matrix[seq][x] = result
 
         print("")
         return matrix
@@ -111,25 +124,68 @@ class Markov:
         return transitions, sequences
 
     def generate_word(self, model, seed):
-        transitions,sequences = model
+        transitions, sequences = model
         r = random.Random()
         r.seed(seed)
         currentState = 0
         currentIteration = 0
+        finished = False
         result = ""
-        while True:
+
+        while not finished:
             if currentState == 0 and currentIteration > 0:
                 break
+
             currentIteration += 1
             randomNumber = r.random()
+
             for i in range(len(transitions[currentState])):
                 if randomNumber < transitions[currentState][i]:
+                    if (sequences[i] == '$'):
+                        finished = True
+                        break
                     result += sequences[i]
                     currentState = i
+                    break
+
                 else:
                     randomNumber -= transitions[currentState][i]
 
         return result
 
-    def get_probability(self, model, word):
-        print("")
+    def get_probability(self, model, word:str):
+        transitions, sequences = model
+        multResult = transitions
+        probabilitiesArray = []
+        sequenceSize = len(sequences[0])
+
+        for i in range(100):
+            multResult = np.matmul(multResult, transitions)
+
+        result = multResult[0]
+        lowerWord = word.lower()
+        decoratedWord = lowerWord
+        # decoratedWord = self.add_decorators_string(lowerWord, '$', 1)
+
+        for k in range(len(decoratedWord)):
+            try:
+                currentSequence = decoratedWord[k:k + sequenceSize]
+                probabilitiesArray.append(result[self.findSequenceIndex(currentSequence, sequences)])
+            except:
+                continue
+
+        total = probabilitiesArray[0]
+        for i in range (1, len(probabilitiesArray)):
+            total *= probabilitiesArray[i]
+
+        return total
+
+    def findSequenceIndex(self, seq, sequences):
+        counter = 0
+
+        for item in sequences:
+            if item == seq:
+                break
+            counter += 1
+
+        return counter
