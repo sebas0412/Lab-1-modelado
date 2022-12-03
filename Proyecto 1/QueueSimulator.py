1b199c358cbd32e22b410ec831ed5a65569a637a
Gerardo Campos <gerardo.camposbadilla@ucr.ac.cr>
2022-12-03 15:25:04 -0600

    Puto el que lo lea

--- Proyecto 1/QueueSimulator.py
+++ Proyecto 1/QueueSimulator.py
@@ -4,7 +4,7 @@ import numpy as np
 print("Hello world")
 
 class Client:
-    def __init__(self,horaLlegada,horaAtencion,horaSalida):
+    def __init__(self, horaLlegada, horaAtencion, horaSalida):
         self.horaLlegada = horaLlegada
         self.horaAtencion = horaAtencion
         self.horaSalida = horaSalida
@@ -12,9 +12,11 @@ class Client:
         return f"Hora de llegada: {self.horaLlegada} Hora de Atencion: {self.horaAtencion} Hora de salida: {self.horaSalida}"
 
 class Server:
-    def __init__(self,clienteActual, offtime):
-        self.clienteActual = clienteActual
-        self.offtime = offtime
+    offtime: int
+    serverQueue: []
+    def __init__(self, queue):
+        self.offtime = 0
+        self.serverQueue = queue
     def __str__(self):
         return f"{self.offtime}"
 
@@ -26,6 +28,8 @@ class Queue:
     service: str        #Distribucion tiempo de salida
     mu: float           #Tasa de servicio de clientes dado N
     contador: int
+    queue: []
+    servidores = []
 
     def __init__(self, lmax, s, arrival, lambd, service, mu):
         self.lmax = lmax
@@ -37,7 +41,8 @@ class Queue:
         self.contador = 0
 
     def simulation(self, time_limit, initial_clients, maximum_arrivals):
-
+        for i in range(self.s):
+            self.servidores.append(Server(self.queue))
         print()
 
         #Imprimir total de clientes servidos
@@ -45,17 +50,17 @@ class Queue:
         #Tiempo de espera promedio de la cola (Qw)
         #Tiempo de servicio Promedio (Lw)
         #Tiempo primedio ocioso   p = lambda/mu, 1-p
+
     def calcularLambda(self, n: int):
         return 64 - n ** 1.5
 
     def calcularMu(self, n: int):
         return 5 + 3 * n
 
-    def incrementarContador(self):
-        self.contador += 1
-        self.agregarClienteALaCola()
     def agregarClienteALaCola(self):
         print()
+
+
 
 def degenerate(lambd):
     return lambd
@@ -64,6 +69,23 @@ def markovian(lambd):
     exponential = np.random.exponential(lambd, 1)
     return exponential
 
-print(markovian(0.864))
+print(markovian(1))
+print(markovian(1))
+print(markovian(1))
+print(markovian(1))
+print(markovian(1))
+print(markovian(1))
+print(markovian(1))
+print(markovian(1))
+print(markovian(1))
+print(markovian(1))
+print(markovian(1))
+print(markovian(1))
+print(markovian(1))
+print(markovian(1))
+print(markovian(1))
+print(markovian(1))
+print(markovian(1))
+print(markovian(1))
+print(markovian(1))
 cliente1 = Client(2,7,7)
-print(Server(cliente1, 822))
